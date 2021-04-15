4c07cfc9453a8bfe790d7c8cf166818f8245f5a9
Blazej Jurczak <blazej7887@gmail.com>
2021-04-15 14:57:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#zad 1
+# a = 4*np.arange(20)
+# print(a)
+
+#zad 2
+# a = np.arange(10, dtype='float')
+# print(a)
+# b = a.astype('int32')
+# print(b)
+
+#zad 3
+# def zad3(n : int):
+#     a = 2**np.mgrid[1:n+1, 1:n+1]
+#     return a
+# print(zad3(5))
+
+#zad4
